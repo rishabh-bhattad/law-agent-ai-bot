64e5c208951836eb9590704b3f981c525196be8a
Rishabh Bhattad <rishabh.bhattad@gmail.com>
2026-08-27 13:27:02 -0700

    Add CourtListener integration and update .gitignore

---
+++ app/integrations/courtlistener.py
@@ -0,0 +1,23 @@
+import httpx
+
+from app.core.config import settings
+
+class CourtListenerClient:
+    def __init__(self):
+        self.url = settings.COURTLISTENER_BASE_URL
+        self.headers = {
+            "Authorization": f"Token {settings.COURTLISTENER_API_KEY}"
+        }
+
+    async def search_opinions(self, query: str):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                url=f"{self.url}/search/",
+                headers=self.headers,
+                params={
+                    "q": query
+                }
+            )
+            response.raise_for_status()
+
+            return response.json()
